254abb519a85a810298be19eee5219a54ed477be
Florencio <florencio.pool@hotmail.com>
2025-09-29 17:01:03 -0600

    initial commit

---
+++ src/main/java/com/resappgcp/restgcp/RestgcpApplication.java
@@ -0,0 +1,13 @@
+package com.resappgcp.restgcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestgcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestgcpApplication.class, args);
+	}
+
+}
